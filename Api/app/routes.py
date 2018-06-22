89cd33c58c72d521df6b8d0d15044c20b155c97a
Larry karani <karanilarry@gmail.com>
2018-06-22 11:47:16 +0300

    Revert "add project stracture finishes[#158525635] [#158525601]"
    This reverts commit 7f4a7e6b9ce9d888a73f8bf10bc4299f22d00de2.

--- Api/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import request
-from flask_restful import Resource
-
-#local imports
-import json
-
-class TestApi(Resource):
-    def get(self):
-        return{'hello':'kenya'}, 200
--- Api/manage.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-import os
-
-environment = os.environ.get('appconfig')
-app = create_app(environment)
-
-if __name__ == '__main__':
-    app.run()
